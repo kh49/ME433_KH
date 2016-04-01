d362576f6e8a9b9c4a323c6777f0171c5b0b256c
Kai Huang <kaihuang2016@u.northwestern.edu>
2016-04-01 11:36:16 -0500

    Plib install
    installed old plib and implemented systemconfigperformance

--- HW1.X/main.c
+++ HW1.X/main.c
@@ -1,4 +1,5 @@
 #include <xc.h>
+#include <plib.h>
 // DEVCFG0
 #pragma config JTAGEN = ON             // JTAG Enable (JTAG Disabled)
 #pragma config ICESEL = ICS_PGx1        // ICE/ICD Comm Channel Select (Communicate on PGEC1/PGED1)
@@ -6,20 +7,25 @@
 #pragma config BWP = OFF                // Boot Flash Write Protect bit (Protection Disabled)
 #pragma config CP = OFF                 // Code Protect (Protection Disabled)
 
-#pragma config FNOSC = FRCPLL
+#pragma config FNOSC = PRIPLL
 #pragma config FSOSCEN = OFF
-#pragma config POSCMOD = EC
-#pragma config OSCIOFNC = ON
+#pragma config IESO = OFF
+#pragma config POSCMOD = XT
+#pragma config OSCIOFNC = OFF
 #pragma config FPBDIV = DIV_1
 #pragma config FPLLIDIV = DIV_2
 #pragma config FPLLMUL = MUL_24
 #pragma config FPLLODIV = DIV_2
 #pragma config UPLLIDIV = DIV_2
 #pragma config UPLLEN = ON
+
+#define SYS_FREQ (48000000)
+
 void delay(void);
 
 void main() {
-    TRISA = 0xFFCF;   
+    SYSTEMConfigPerformance(48000000);
+  TRISA = 0xFFCF;   
     while(1) {
        
         PORTAINV = 0x0010;
@@ -30,7 +36,7 @@ void main() {
 
 void delay(void) {
     int j;
-    for (j=0; j<4000000; j++) {
+    for (j=0; j<48000; j++) {
         
     }
 }
