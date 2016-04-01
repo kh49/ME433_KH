b50fe216edbd6ddcd85d9d71f69be46ff1599e35
Kai Huang <kaihuang2016@u.northwestern.edu>
2016-03-31 23:39:01 -0500

    Added TRISA setting to set RA4 and RA5 as outputs
    while loop to turn on LED

--- HW1.X/main.c
+++ HW1.X/main.c
@@ -1,4 +1,10 @@
 #include <xc.h>
+// DEVCFG0
+#pragma config JTAGEN = ON             // JTAG Enable (JTAG Disabled)
+#pragma config ICESEL = ICS_PGx1        // ICE/ICD Comm Channel Select (Communicate on PGEC1/PGED1)
+#pragma config PWP = OFF                // Program Flash Write Protect (Disable)
+#pragma config BWP = OFF                // Boot Flash Write Protect bit (Protection Disabled)
+#pragma config CP = OFF                 // Code Protect (Protection Disabled)
 
 #pragma config FNOSC = FRCPLL
 #pragma config FSOSCEN = OFF
@@ -12,5 +18,12 @@
 #pragma config UPLLEN = ON
 
 void main() {
-    
+            
+    while(1) {
+
+        TRISA = 0xFFCF;
+        PORTAbits.RA4 = 1;
+        
 }
+}
+
