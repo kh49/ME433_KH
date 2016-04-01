88acbacd72f9a6c84d28a1923d2f4e405170ea2b
Kai Huang <kaihuang2016@u.northwestern.edu>
2016-03-31 19:04:45 -0500

    DEVCFG 1
    configuration output 48Mhz based on microchip spreadsheet calculator

---
+++ HW1.X/main.c
@@ -0,0 +1,12 @@
+#include 
+
+#pragma config FNOSC = FRCPLL
+#pragma config FSOSCEN = OFF
+#pragma config POSCMOD = EC
+#pragma config OSCIOFNC = ON
+#pragma config FPBDIV = DIV_1
+#pragma config FPLLIDIV = DIV_2
+#pragma config FPLLMUL = MUL_24
+#pragma config FPLLODIV = DIV_2
+#pragma config UPLLIDIV = DIV_2
+#pragma config UPLLEN = ON
